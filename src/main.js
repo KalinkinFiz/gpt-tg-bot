d4e2b4cde4d49d47146ea4d5e435d12eab3b16c0
KalinkinFiz <kalinkin.fiz@gmail.com>
2023-04-26 12:36:13 +0300

    feat: add OggConverter

--- src/main.js
+++ src/main.js
@@ -2,13 +2,17 @@ import { Telegraf } from "telegraf";
 import { message } from "telegraf/filters"
 import config from "config"
 
+import { ogg } from "./ogg.js"
+
 const bot = new Telegraf(config.get('TELEGRAM_TOKEN'))
 
 bot.on(message('voice'), async ctx => {
     try {
         const link = await ctx.telegram.getFileLink(ctx.message.voice.file_id)
-        console.log(link);
-        await ctx.reply(JSON.stringify(link, null, 2))
+        const uesrId = String(ctx.message.from.id)
+        const oggPath = await ogg.create(link.href, uesrId)
+        const mp3Path = await ogg.toMp3(oggPath, uesrId)
+        await ctx.reply(mp3Path)
     } catch (e) {
         console.log(`Error while voice message`, e.message);
     }
@@ -19,7 +23,7 @@ bot.command('start', async (ctx) => {
     await ctx.reply(JSON.stringify(ctx.message, null, 2))
 })
 
-bot.launch
+bot.launch()
 
 process.once("SIGINT", () => bot.stop("SIGINT"))
 process.once("SIGTERM", () => bot.stop("SIGTERM"))
