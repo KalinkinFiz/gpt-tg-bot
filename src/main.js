13d6bc2a1e6ea894f58aad3e96cdcc7af66fe62b
KalinkinFiz <kalinkin.fiz@gmail.com>
2023-04-27 00:32:11 +0300

    feat: add session

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { Telegraf } from "telegraf";
+import { Telegraf, session } from "telegraf";
 import { message } from "telegraf/filters"
 import { code } from "telegraf/format"
 import config from "config"
@@ -6,9 +6,26 @@ import config from "config"
 import { ogg } from "./ogg.js"
 import { openai } from "./openai.js"
 
+const INITIAL_SESSION = {
+    messages: []
+}
+
 const bot = new Telegraf(config.get('TELEGRAM_TOKEN'))
 
+bot.use(session())
+
+bot.command('new', async ctx => {
+    ctx.session = INITIAL_SESSION
+    await ctx.reply('Жду вашего голосового или текстового сообщения ...')
+})
+
+bot.command('start', async ctx => {
+    ctx.session = INITIAL_SESSION
+    await ctx.reply('Жду вашего голосового или текстового сообщения ...')
+})
+
 bot.on(message('voice'), async ctx => {
+    ctx.session ??= INITIAL_SESSION // undefined or null
     try {
         await ctx.reply(code('Сообщение принято. Жду ответ от сервера ...'))
         const link = await ctx.telegram.getFileLink(ctx.message.voice.file_id)
@@ -19,18 +36,17 @@ bot.on(message('voice'), async ctx => {
         const text = await openai.transcription(mp3Path)
         await ctx.reply(code(`Ваш запрос: ${text}`))
 
-        const messages = [{ role: openai.roles.USER, content: text }]
-        const response = await openai.chat(messages)
+        ctx.session.messages.push({ role: openai.roles.USER, content: text })
+
+        const response = await openai.chat(ctx.session.messages)
+
+        ctx.session.messages.push({ role: openai.roles.ASSISTANT, content: response.content })
 
         await ctx.reply(response.content)
     } catch (e) {
         console.log(`Error while voice message`, e.message);
     }
 
-})
-
-bot.command('start', async (ctx) => {
-    await ctx.reply(JSON.stringify(ctx.message, null, 2))
 })
 
 bot.launch()
